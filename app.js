6798161361af1f782b2ad40bd0ff1c77ea9881ab
Linda Ramos <ltramos7@gmail.com>
2021-01-03 15:32:07 -0600

    feat: set up express server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.use(express.static('src'));
+
+app.get('/', (req, res)=>{
+    res.sendFile('./src/index.html', {root: __dirname});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
