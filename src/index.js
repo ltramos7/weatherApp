0baf9214079e92159ed0435f1fb81e8018c94b1e
Linda Ramos <ltramos7@gmail.com>
2021-01-03 19:21:48 -0600

    feat: log weather info based on users latitude and longitude

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@
 // new object recieved from weather API
 // define function to display recieved object to display its info onto the frontend
 
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid=13a84a0bd9147d91ae6b48dc7b2619d5
+
 
 
 const currentWeatherBtn = document.getElementById("current-weather")
@@ -12,14 +14,19 @@ const loadStatus = document.getElementById("load-status")
 const mapLink = document.getElementById("map-link")
 
 
-findUserLocation = () => {
 
+
+
+findUserLocation = () => {
+    
     successCallback = (position) => {
         const latitude = position.coords.latitude
         const longitude = position.coords.longitude
         loadStatus.textContent = ""
         mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`
         mapLink.textContent = `You location  Latitude: ${latitude} ° and Longitude: ${longitude} °`;
+
+        fetchLocationWeather(latitude, longitude)
     }
     
     errorCallback = () => {
@@ -32,6 +39,19 @@ findUserLocation = () => {
         loadStatus.textContent = "Locating..."
         navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
     }
+}
+
+fetchLocationWeather = (latitude, longitude) => {
+    console.log("fetch location info function:", latitude, longitude)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=aa662ed0d71b09fd88e5880bc7bf3617`)
+    .then(resp => resp.json() )
+    .then(data => renderLocationInfo(data))
+}
+
+renderLocationInfo = (data) => {
+    console.log("all data info: ", data)
+    console.log("data main", data.main)
+    console.log("name: ", data.name)
 }
 
 currentWeatherBtn.addEventListener("click", findUserLocation)
